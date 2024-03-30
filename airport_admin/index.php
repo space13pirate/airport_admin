a6fd12e30be9289c009c1b27f2c4d72c3bef94a2
Arriannaa <a.najmushina19.ru>
2024-03-30 14:41:05 +0300

    ГЛ: Добавлена главная страница

--- airport_admin/index.php
+++ airport_admin/index.php
@@ -1,78 +1,89 @@
-<?php
-include "app/database/db.php";
-?>
+<!DOCTYPE HTML>
 
-<!doctype html>
-<html lang="en">
+<html>
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/014c37252f.js" crossorigin="anonymous"></script>
-
-    <!-- Custom Styling-->
-    <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-
-    <title>AIRPORT_ADMIN demo</title>
-    <link rel="icon" href=https://i.ibb.co/09w47pS/logo.png type="image/x-icon">
+    <!-- Иконка вкладки браузера -->
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+    <!-- Заголовок вкладки браузера -->
+    <title>Аэропортал</title>
+    <!-- Кодировка символов -->
+    <meta charset="utf-8"/>
+    <!-- Настройки для корректного отображения на мобильных устройствах -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+    <!-- Подключение основных стилей сайта -->
+    <link rel="stylesheet" href="includes/css/main.css"/>
+    <!-- Стили для пользователей с отключенным JavaScript -->
+    <noscript>
+        <link rel="stylesheet" href="includes/css/noscript.css"/>
+    </noscript>
 </head>
-<body>
 
-<!-- Блок хедера -->
-<?php include("app/include/header-user.php"); ?>
+<body class="is-preload">
 
-<!-- Блок карусели - START -->
-<div id="carouselExampleDark" class="carousel carousel-dark slide">
-    <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="10000">
-            <img src="assets/images/p_1.png" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5><a href="">Slide label</a></h5>
-                <p>Some representative placeholder content for the first slide.</p>
-            </div>
+<!-- Оболочка страницы -->
+<div id="page-wrapper">
+
+    <!-- Header -->
+    <header id="header" class="alt">
+        <h1><a href="index.php">АэроПортал: Пассажирские Услуги и Управление Аэропортом</a></h1>
+        <nav>
+            <a href="#menu">Меню</a>
+        </nav>
+    </header>
+
+    <!-- Меню -->
+    <nav id="menu">
+        <div class="inner">
+            <h2>Меню</h2>
+            <ul class="links">
+                <li><a href="index.php">Главная страница</a></li>
+                <li><a href="pass-login.php">Вход для пассажира</a></li>
+                <li><a href="admin/admin-login.php">Вход для администратора</a></li>
+            </ul>
         </div>
-        <div class="carousel-item" data-bs-interval="2000">
-            <img src="assets/images/p_2.png" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5><a href="">Slide label</a></h5>
-                <p>Some representative placeholder content for the second slide.</p>
-            </div>
+    </nav>
+
+    <!-- Banner -->
+    <section id="banner">
+        <div class="inner">
+            <div class="logo"><span class="icon solid fa-plane"></span></div>
+            <h3>Пассажирские Услуги и Управление Аэропортом</h3>
+            <h2>АэроПортал: от входа до взлёта</h2>
+            <p>Лучший, простой и эффективный способ забронировать рейс на самолёт</a></p>
         </div>
-        <div class="carousel-item">
-            <img src="assets/images/p_3.png" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5><a href="">Slide label</a></h5>
-                <p>Some representative placeholder content for the third slide.</p>
-            </div>
+    </section>
+
+    <!-- Footer -->
+    <section id="footer">
+        <div class="inner">
+            <h2 class="major">как с нами связаться</h2>
+
+            <ul class="contact">
+                <li class="icon solid fa-graduation-cap">Казанский Государственный Энергетический университет</li>
+                <li class="icon solid fa-envelope"><a href="mailto:a.najmushina19@mail.ru">a.najmushina19@mail.ru</a>
+                </li>
+                <li class="icon solid fa-envelope"><a href="mailto:north13anastasia@gmail.com">north13anastasia@gmail.com</a>
+                </li>
+                <li class="icon brands fa-github"><a href="https://github.com/Arriannaa">github.com/Arriannaa</a></li>
+                <li class="icon brands fa-github"><a
+                            href="https://github.com/space13pirate">github.com/space13pirate</a></li>
+            </ul>
+            <ul class="copyright">
+                <li>&copy; АРМ администратора аэропорта</li>
+                <li>Разработчики: Арина Наймушина, Анастасия Черемисова</li>
+            </ul>
         </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
+    </section>
+
 </div>
-<!-- Блок карусели - END-->
 
-<!-- Блок футера -->
-<?php include("app/include/footer-bottom.php"); ?>
-
-</body>
-</html>
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<!-- Подключение скриптов для функционала сайта -->
+<script src="includes/js/jquery.min.js"></script>
+<script src="includes/js/jquery.scrollex.min.js"></script>
+<script src="includes/js/browser.min.js"></script>
+<script src="includes/js/breakpoints.min.js"></script>
+<script src="includes/js/util.js"></script>
+<script src="includes/js/main.js"></script>
 
 </body>
 </html>
